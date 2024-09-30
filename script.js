20923520336ac04bf601ee24448e2f6527deccda
wafamahmud <wafamahmud8212@gmail.com>
2024-09-30 23:24:47 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function myfunction() {
+    document.getElementById("r").innerHTML="You are now checking my project";
+    document.getElementById("r1").innerHTML="HI are you still here? ";
+    document.getElementById("demo").firstChild.nodeValue;
+}
